49c7778fe07866ea45e8ec09347a003a92dc78d3
ayesha-mh <ayesha.mohsina@g.bracu.ac.bd>
2025-12-12 02:15:11 +0600

    Add files via upload

---
+++ app/Models/Property.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Property extends Model
+{
+    use HasFactory;
+
+    // Add all the fields you want to fill via $fillable
+    protected $fillable = [
+        'title',
+        'description',
+        'price',
+        'address',
+        'city',
+        'neighborhood',
+        'property_type',
+        'bedrooms',
+        'bathrooms',
+        'area',
+        'available_date',
+        'lease_term',
+        'security_deposit',
+        'parking',
+        'laundry',
+        'pet_policy',
+        'furnished',
+        'contact_email',
+        'contact_phone',
+        'user_id'
+    ];
+
+    // Relationship with images
+    public function images()
+    {
+        return $this->hasMany(PropertyImage::class);
+    }
+}
